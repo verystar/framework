e1dfb2bd451f4451851c2e4bb236380f0b0533b4
caixudong <caixudong@verystar.cn>
2016-06-03 17:57:53 +0800

    php cs fixer

--- SSDB.php
+++ SSDB.php
@@ -1,6 +1,8 @@
-<?php namespace Very\Cache;
+<?php
 
-/**
+namespace Very\Cache;
+
+/*
  * Created by JetBrains PhpStorm.
  * User: CAIXUDONG
  * Date: 13-1-22
@@ -9,15 +11,15 @@
 
 use Very\Library\FStat;
 
-class SSDB {
-
+class SSDB
+{
     //https://github.com/jonnywang/phpssdb/wiki
     //注意lpop rpop属于写操作
     private $write_methods = array(
-        'set' => 1, 'setx' => 1, 'setnx' => 1, 'expire' => 1, 'ttl' => 1, 'getset' => 1, 'del' => 1, 'incr' => 1, 'setbit' => 1, 'multi_set' => 1, 'multi_del' => 1, 'hset' => 1, 'hdel' => 1, 'hincr' => 1, 'hclear' => 1, 'multi_hset' => 1, 'multi_hdel' => 1, 'qclear' => 1, 'qpush' => 1, 'qpush_back' => 1, 'qpush_front' => 1, 'qpop' => 1, 'qpop_back' => 1, 'qpop_front' => 1, 'qfront' => 1, 'qback' => 1, 'qget' => 1, 'qset' => 1, 'qtrim_front' => 1, 'qtrim_back' => 1, 'geo_set' => 1, 'geo_del' => 1, 'geo_clear' => 1
+        'set' => 1, 'setx' => 1, 'setnx' => 1, 'expire' => 1, 'ttl' => 1, 'getset' => 1, 'del' => 1, 'incr' => 1, 'setbit' => 1, 'multi_set' => 1, 'multi_del' => 1, 'hset' => 1, 'hdel' => 1, 'hincr' => 1, 'hclear' => 1, 'multi_hset' => 1, 'multi_hdel' => 1, 'qclear' => 1, 'qpush' => 1, 'qpush_back' => 1, 'qpush_front' => 1, 'qpop' => 1, 'qpop_back' => 1, 'qpop_front' => 1, 'qfront' => 1, 'qback' => 1, 'qget' => 1, 'qset' => 1, 'qtrim_front' => 1, 'qtrim_back' => 1, 'geo_set' => 1, 'geo_del' => 1, 'geo_clear' => 1,
     );
     private $read_methods = array(
-        'ping' => 1, 'version' => 1, 'dbsize' => 1, 'info' => 1, 'get' => 1, 'exists' => 1, 'getbit' => 1, 'countbit' => 1, 'substr' => 1, 'strlen' => 1, 'keys' => 1, 'scan' => 1, 'smembers' => 1, 'rscan' => 1, 'multi_get' => 1, 'hget' => 1, 'hexists' => 1, 'hsize' => 1, 'hlist' => 1, 'hrlist' => 1, 'hkeys' => 1, 'hgetall' => 1, 'hscan' => 1, 'hrscan' => 1, 'multi_hget' => 1, 'qsize' => 1, 'qlist' => 1, 'qrlist' => 1, 'geo_get' => 1, 'geo_neighbour' => 1, 'geo_distance' => 1
+        'ping' => 1, 'version' => 1, 'dbsize' => 1, 'info' => 1, 'get' => 1, 'exists' => 1, 'getbit' => 1, 'countbit' => 1, 'substr' => 1, 'strlen' => 1, 'keys' => 1, 'scan' => 1, 'smembers' => 1, 'rscan' => 1, 'multi_get' => 1, 'hget' => 1, 'hexists' => 1, 'hsize' => 1, 'hlist' => 1, 'hrlist' => 1, 'hkeys' => 1, 'hgetall' => 1, 'hscan' => 1, 'hrscan' => 1, 'multi_hget' => 1, 'qsize' => 1, 'qlist' => 1, 'qrlist' => 1, 'geo_get' => 1, 'geo_neighbour' => 1, 'geo_distance' => 1,
     );
 
     //是否加入统计队列，因为刷新内存的代码并不需要加入统计队列，否则统计队列会撑挂掉
@@ -32,13 +34,15 @@ class SSDB {
      * @param $singleton
      *
      * @return mixed
+     *
      * @author 蔡旭东 mailto:fifsky@dev.ppstream.com
      */
-    static public function getInstance($node, $singleton = true) {
+    public static function getInstance($node, $singleton = true)
+    {
         static $cache = array();
 
         if (!isset($cache[$node]) || !$singleton) {
-            $cache[$node]         = new self();
+            $cache[$node] = new self();
             $cache[$node]->config = config('ssdb', $node);
         }
 
@@ -46,14 +50,16 @@ class SSDB {
     }
 
     //是否开启监控统计
-    public function setStat($is_stat) {
+    public function setStat($is_stat)
+    {
         $this->is_stat = $is_stat;
+
         return $this;
     }
 
-
-    private function connect($func) {
-        $func   = strtolower($func);
+    private function connect($func)
+    {
+        $func = strtolower($func);
         $config = $this->config[array_rand($this->config)];
 
         if (!$config) {
@@ -77,7 +83,7 @@ class SSDB {
         //保持唯一连接
         list($ip, $port, $dbnum) = explode(':', $server);
 
-        $this->ip   = $ip;
+        $this->ip = $ip;
         $this->port = $port;
 
         static $instance_cache = array();
@@ -90,7 +96,6 @@ class SSDB {
             try {
                 $_start_time = microtime(true);
 
-
                 if (class_exists('\SSDB', false)) {
                     $instance_cache[$server] = new \SSDB();
                 } else {
@@ -100,23 +105,25 @@ class SSDB {
                 $instance_cache[$server]->connect($ip, $port, 1);
 
                 if ($this->is_stat) {
-                    $_stat->set(1, 'SSDB连接效率', $_stat->formatTime(number_format(microtime(true) - $_start_time, 6)), $ip . ':' . $port);
+                    $_stat->set(1, 'SSDB连接效率', $_stat->formatTime(number_format(microtime(true) - $_start_time, 6)), $ip.':'.$port);
                 }
             } catch (SSDBException $e) {
                 if ($this->is_stat) {
                     $_stat->set(1, 'BUG错误', 'SSDB连接错误', "{$ip}:{$port}");
                 }
-                throw new SSDBException('SSDB connect error:' . $e->getMessage());
+                throw new SSDBException('SSDB connect error:'.$e->getMessage());
             }
         }
 
         return $instance_cache[$server];
     }
 
-    private function isConnectionLost(SSDBException $e) {
+    private function isConnectionLost(SSDBException $e)
+    {
         if (strpos($e->getMessage(), 'SSDB server went away') !== false || strpos($e->getMessage(), 'Connection lost') !== false || strpos($e->getMessage(), 'Connection refused') !== false) {
             return true;
         }
+
         return false;
     }
 
@@ -125,19 +132,20 @@ class SSDB {
      * @param $params
      *
      * @return mixed
+     *
      * @throws SSDBException
      */
-    public function __call($func, $params) {
+    public function __call($func, $params)
+    {
         if ($this->is_stat) {
             $_stat = FStat::getInstance();
         }
 
         $redis_server = $this->connect($func);
-        for ($i = 0; $i < 2; $i++) {
-
+        for ($i = 0; $i < 2; ++$i) {
             try {
                 $_start_time = microtime(true);
-                $ret         = call_user_func_array(array($redis_server, $func), $params);
+                $ret = call_user_func_array(array($redis_server, $func), $params);
                 if ($this->is_stat) {
                     $_stat->set(1, 'SSDB执行效率', $_stat->formatTime(number_format(microtime(true) - $_start_time, 6)), "{$this->ip}:{$this->port}({$func})");
                 }
@@ -149,7 +157,7 @@ class SSDB {
                     $redis_server = $this->connect($func);
                     continue;
                 } else {
-                    throw new SSDBException('SSDB execute error:' . $e->getMessage());
+                    throw new SSDBException('SSDB execute error:'.$e->getMessage());
                 }
             }
             break;
