673281a5bcfa6e50629ad9dd0f3e685ddc1f54db
caixudong <caixudong@verystar.cn>
2016-06-03 22:07:05 +0800

    添加通用traits方法

---
+++ src/Very/Traits/Singleton.php
@@ -0,0 +1,26 @@
+<?php
+namespace Very\Traits;
+
+/**
+ * Created by PhpStorm.
+ * User: 蔡旭东 caixudong@verystar.cn
+ * Date: 6/3/16 21:58
+ * The singleton trait.
+ *
+ * @see   http://en.wikipedia.org/wiki/Singleton_pattern
+ * @since 1.0.0
+ */
+trait Singleton
+{
+    /**
+     * Returns the only instance of the Singleton class.
+     *
+     * @return static the only instance of the Singleton class.
+     */
+    public static function getInstance()
+    {
+        static $_instance = null;
+
+        return $_instance ? $_instance : $_instance = new static();
+    }
+}
