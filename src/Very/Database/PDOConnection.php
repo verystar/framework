cf4b6f7a700fc4595f46a6a07f1bf9ab9e4b82a8
caixudong <caixudong@verystar.cn>
2016-06-13 11:37:32 +0800

    fix delete

--- src/Very/Database/PDOConnection.php
+++ src/Very/Database/PDOConnection.php
@@ -27,13 +27,15 @@ class PDOConnection extends PDO
     public function connect($dsn, $username, $password, $driver_options = array())
     {
         try {
-            $start_time = microtime(true);
-            $this->dsn = $dsn;
-            $this->username = $username;
-            $this->password = $password;
+            $start_time           = microtime(true);
+            $this->dsn            = $dsn;
+            $this->username       = $username;
+            $this->password       = $password;
             $this->driver_options = $driver_options;
-            parent::__construct($dsn, $username, $password, array(PDO::ATTR_CASE => PDO::CASE_LOWER,
-                                                                  PDO::ATTR_TIMEOUT => 1, ) + $driver_options);
+            parent::__construct($dsn, $username, $password, array(
+                    PDO::ATTR_CASE    => PDO::CASE_LOWER,
+                    PDO::ATTR_TIMEOUT => 1,
+                ) + $driver_options);
             $this->connect_time = number_format(microtime(true) - $start_time, 6);
 
             if ($this->is_stat) {
@@ -46,7 +48,7 @@ class PDOConnection extends PDO
         } catch (PDOException $e) {
             //监控SQL连接错误
             FStat::getInstance()->set(1, 'BUG错误', 'SQL连接错误', $dsn, $e->getMessage());
-            throw new \PDOException("DB connect error for dns $dsn:".$e->getMessage());
+            throw new \PDOException("DB connect error for dns $dsn:" . $e->getMessage());
         }
     }
 
@@ -54,19 +56,19 @@ class PDOConnection extends PDO
     {
         $sql .= ' limit :limit_from,:limit';
 
-        return $this->getAll($sql, array_merge($params, array('limit' => (int) $limit, 'limit_from' => (int) $limit_from)), $driver_options);
+        return $this->getAll($sql, array_merge($params, array('limit' => (int)$limit, 'limit_from' => (int)$limit_from)), $driver_options);
     }
 
     //返回数据以及是否含有下一页
     public function getPager($sql, $page, $num = 0, $params = array(), $driver_options = array())
     {
         $sql .= ' limit :limit_from,:limit';
-        $ret = $this->getAll($sql, array_merge($params, array('limit' => (int) $num + 1, 'limit_from' => ($page - 1) * (int) $num)), $driver_options);
+        $ret   = $this->getAll($sql, array_merge($params, array('limit' => (int)$num + 1, 'limit_from' => ($page - 1) * (int)$num)), $driver_options);
         $count = count($ret);
         $count > $num && array_pop($ret);
 
         return array(
-            'rs' => $ret,
+            'rs'   => $ret,
             'next' => $count > $num ? true : false,
         );
     }
@@ -74,7 +76,7 @@ class PDOConnection extends PDO
     public function getAll($sql, $params = array(), $driver_options = array(), $fetch_style = PDO::FETCH_ASSOC)
     {
         $stmt = $this->execute($sql, $params, $driver_options);
-        $all = $stmt->fetchAll($fetch_style);
+        $all  = $stmt->fetchAll($fetch_style);
 
         return $all;
     }
@@ -82,7 +84,7 @@ class PDOConnection extends PDO
     public function getOneAll($sql, $params = array(), $column_number = 0, $driver_options = array())
     {
         $stmt = $this->execute($sql, $params, $driver_options);
-        $all = array();
+        $all  = array();
         while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
             $all[] = $row[$column_number];
         }
@@ -93,7 +95,7 @@ class PDOConnection extends PDO
     public function getRow($sql, $params = array(), $driver_options = array(), $fetch_style = PDO::FETCH_ASSOC)
     {
         $stmt = $this->execute($sql, $params, $driver_options);
-        $row = $stmt->fetch($fetch_style);
+        $row  = $stmt->fetch($fetch_style);
 
         return $row;
     }
@@ -101,7 +103,7 @@ class PDOConnection extends PDO
     public function getOne($sql, $params = array(), $driver_options = array(), $column_number = 0)
     {
         $stmt = $this->execute($sql, $params, $driver_options);
-        $one = $stmt->fetchColumn($column_number);
+        $one  = $stmt->fetchColumn($column_number);
 
         return $one;
     }
@@ -110,8 +112,8 @@ class PDOConnection extends PDO
     {
         for ($i = 0; $i < 2; ++$i) {
             $start_time = microtime(true);
-            $stmt = $this->prepare($sql, $driver_options);
-            $this->sql = $sql;
+            $stmt       = $this->prepare($sql, $driver_options);
+            $this->sql  = $sql;
 
             if ($stmt) {
                 foreach ($params as $k => &$param) {
@@ -120,10 +122,10 @@ class PDOConnection extends PDO
                 $stmt->execute();
             }
 
-            $end_time = microtime(true);
+            $end_time           = microtime(true);
             $this->execute_time = number_format($end_time - $start_time, 6);
-            $this->error_code = $stmt ? $stmt->errorCode() : $this->errorCode();
-            $this->error_info = $stmt ? $stmt->errorInfo() : $this->errorInfo();
+            $this->error_code   = $stmt ? $stmt->errorCode() : $this->errorCode();
+            $this->error_info   = $stmt ? $stmt->errorInfo() : $this->errorInfo();
 
             //在常驻进程中，DB连接会丢失，但是并不会抛出异常，因此需要判断错误的值为2006就需要重新连接DB，然后再次执行query
             if ($this->isError() && $this->error_info[1] == 2006) {
@@ -136,7 +138,7 @@ class PDOConnection extends PDO
 
                 //监控SQL错误
                 if ($this->isError()) {
-                    $debug = $this->debug();
+                    $debug       = $this->debug();
                     $debug['参数'] = $params;
                     logger()->error('SQL Error', $debug);
                     $_stat->set(1, 'BUG错误', 'SQL执行错误', $sql, json_encode($this->getErrorInfo()), 100);
@@ -156,10 +158,10 @@ class PDOConnection extends PDO
     public function debug()
     {
         return [
-            'DSN' => $this->dsn,
-            'SQL' => $this->sql,
-            '状态' => $this->getErrorCode(),
-            '错误' => $this->getErrorInfo(),
+            'DSN'  => $this->dsn,
+            'SQL'  => $this->sql,
+            '状态'   => $this->getErrorCode(),
+            '错误'   => $this->getErrorInfo(),
             '连接耗时' => $this->getConnTime(),
             '执行耗时' => $this->getExecTime(),
         ];
@@ -182,7 +184,7 @@ class PDOConnection extends PDO
         $sets = array();
         foreach ($params as $k => $v) {
             if (!in_array($k, $where_field)) {
-                $sets[] = ' `'.$k.'` =:'.$k;
+                $sets[] = ' `' . $k . '` =:' . $k;
             }
         }
 
@@ -204,9 +206,9 @@ class PDOConnection extends PDO
             return false;
         }
 
-        $keys = array_keys($params);
-        $fileds = '`'.implode('`,`', $keys).'`';
-        $filed_values = ':'.implode(',:', $keys);
+        $keys         = array_keys($params);
+        $fileds       = '`' . implode('`,`', $keys) . '`';
+        $filed_values = ':' . implode(',:', $keys);
 
         $sql = "insert into {$table}($fileds) values($filed_values)";
         $ret = $this->execute($sql, $params);
@@ -219,7 +221,7 @@ class PDOConnection extends PDO
 
     public function delete($table, $where, $params)
     {
-        if (strpos($where, '=') < 1 || !$params) {
+        if (!$params) {
             return false;
         }
 
