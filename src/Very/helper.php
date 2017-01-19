404d42a788df812fff6678e1627c7ef16bd735f9
fifsky <fifsky@aliyun.com>
2017-01-19 10:47:05 +0800

    add retry function

--- src/Very/helper.php
+++ src/Very/helper.php
@@ -870,3 +870,32 @@ if (!function_exists('curl')) {
         return new Very\Support\Curl();
     }
 }
+
+if (! function_exists('retry')) {
+    /**
+     * Retry an operation a given number of times.
+     * @param          $times
+     * @param callable $callback
+     * @param int      $sleep
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    function retry($times, callable $callback, $sleep = 0)
+    {
+        $times--;
+        beginning:
+        try {
+            return $callback();
+        } catch (Exception $e) {
+            if (! $times) {
+                throw $e;
+            }
+            $times--;
+            if ($sleep) {
+                usleep($sleep * 1000);
+            }
+            goto beginning;
+        }
+    }
+}
