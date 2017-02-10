0c64a2d3cf26b3f5317d4bd9ee5e646ecad25154
fifsky <fifsky@aliyun.com>
2017-02-10 22:46:15 +0800

    update UrlGenerator

--- src/Very/Routing/UrlGenerator.php
+++ src/Very/Routing/UrlGenerator.php
@@ -6,7 +6,6 @@ class UrlGenerator
 {
     private $params = [];
     private $url;
-    private $path;
     private $secure;
 
     public function to($path = null, $params = [], $secure = false)
@@ -29,36 +28,33 @@ class UrlGenerator
         $this->secure = $secure;
         $params_index = strpos($path, '?');
         $query        = $params_index === false ? '' : parse_url($path, PHP_URL_QUERY);
-        $this->path   = $params_index === false ? $path : substr($path, 0, $params_index);
+        $path   = $params_index === false ? $path : substr($path, 0, $params_index);
         $path_params  = [];
         if ($query) {
             parse_str($query, $path_params);
         }
 
         $this->params = array_merge($path_params, $params);
-        $this->recombine();
-        return $this;
-    }
 
-    public function url()
-    {
-        return $this->url;
-    }
-
-    /**
-     * Recombine url
-     */
-    private function recombine()
-    {
-        $params_str = $this->params ? '?' . http_build_query($this->params) : '';
-        if (substr($this->path, 0, 4) === 'http') {
-            $this->url = $this->path . $params_str;
+        if (substr($path, 0, 4) === 'http') {
+            $this->url = $path;
         } else {
             $site_root = isset($_SERVER['HTTPS']) || $this->secure ? 'https' : 'http';
             $site_root = isset($_SERVER['HTTP_HOST']) ? $site_root . '://' . $_SERVER['HTTP_HOST'] . '/' : '/';
 
-            $this->url = $site_root . ltrim($this->path, '/') . $params_str;
+            $this->url = $site_root . ltrim($path, '/');
         }
+
+        return $this;
+    }
+
+    /**
+     * return url
+     * @return string
+     */
+    public function url()
+    {
+        return $this->url . ($this->params ? '?' . http_build_query($this->params) : '');
     }
 
     /**
@@ -71,7 +67,6 @@ class UrlGenerator
     public function append($params = [])
     {
         $this->params = array_merge($this->params, $params);
-        $this->recombine();
         return $this;
     }
 
@@ -85,7 +80,6 @@ class UrlGenerator
     public function remove($params = [])
     {
         $this->params = array_diff_key($this->params, array_fill_keys($params, ''));
-        $this->recombine();
         return $this;
     }
 }
