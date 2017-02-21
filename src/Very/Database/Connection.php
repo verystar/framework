b9d979c2d65461718e5d8287e24bf4ac3fa88d40
fifsky <fifsky@aliyun.com>
2017-02-21 20:42:42 +0800

    support password

--- src/Very/Database/Connection.php
+++ src/Very/Database/Connection.php
@@ -30,23 +30,27 @@ class Connection
     public static function getInstance($db)
     {
         if (!isset(self::$instances[$db])) {
-            $config = config('db', $db);
+            $config = config('db.' . $db);
             if ($config) {
                 try {
                     if ($config['dbtype'] == 'mysql') {
-                        $dns = 'mysql:host='.$config['dbhost'].';dbname='.$config['dbname'];
+                        $dns = 'mysql:host=' . $config['dbhost'] . ';dbname=' . $config['dbname'];
 
                         if (isset($config['dbport']) && !empty($config['dbport'])) {
-                            $dns .= ';port='.$config['dbport'];
+                            $dns .= ';port=' . $config['dbport'];
                         }
 
-                        self::$instances[$db] = new MysqlConnection($dns, $config['dbuser'], $config['dbpswd'], array(
-                            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.$config['dbcharset'], ));
+                        self::$instances[$db] = new MysqlConnection($dns, $config['dbuser'], $config['dbpswd']);
+                        $collation = !is_null($config['collation']) ? " collate '{$config['collation']}'" : '';
+
+                        self::$instances[$db]->prepare(
+                            "set names '{$config['charset']}'" . $collation
+                        )->execute();
                     }
 
                     return self::$instances[$db];
                 } catch (PDOException $e) {
-                    throw new RuntimeException("DB connect error for db $db:".$e->getMessage());
+                    throw new RuntimeException("DB connect error for db $db:" . $e->getMessage());
                 }
             } else {
                 throw new RuntimeException('DB config error! Please checking dir in config/db.php');
