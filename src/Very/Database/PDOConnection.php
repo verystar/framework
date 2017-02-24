f90827f66a89dd7002d79d6624f312a09621ff05
fifsky <fifsky@aliyun.com>
2017-02-24 16:27:24 +0800

    pdo errmode change to exception

--- src/Very/Database/PDOConnection.php
+++ src/Very/Database/PDOConnection.php
@@ -21,12 +21,12 @@ class PDOConnection
      * @var array
      */
     protected $options = [
-        PDO::ATTR_CASE       => PDO::CASE_NATURAL,
-//        PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_AUTOCOMMIT => 1,
-//        PDO::ATTR_ORACLE_NULLS      => PDO::NULL_NATURAL,
-//        PDO::ATTR_STRINGIFY_FETCHES => false,
-//        PDO::ATTR_EMULATE_PREPARES  => false,
+        PDO::ATTR_CASE              => PDO::CASE_NATURAL,
+        PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_AUTOCOMMIT        => true,
+        PDO::ATTR_ORACLE_NULLS      => PDO::NULL_NATURAL,
+        PDO::ATTR_STRINGIFY_FETCHES => false,
+        PDO::ATTR_EMULATE_PREPARES  => false,
     ];
 
     /**
