054125966e436ba31b7dfd00bcf0545462949714
fifsky <fifsky@aliyun.com>
2017-03-20 22:50:41 +0800

    config file chage to database

--- src/Very/Database/Connection.php
+++ src/Very/Database/Connection.php
@@ -28,12 +28,12 @@ class Connection
     public static function getInstance($db)
     {
         if (!isset(self::$instances[$db])) {
-            $config = config('db.' . $db);
+            $config = config('database.' . $db);
             if (!$config) {
-                throw new RuntimeException('Not found database config [' . $db . '], Please checking file config/db.php');
+                throw new RuntimeException('Not found database config [' . $db . '], Please checking file config/database.php');
             }
 
-            switch ($config['dbtype']) {
+            switch ($config['driver']) {
                 case 'mysql':
                     self::$instances[$db] = new MysqlConnection($config);
                     break;
