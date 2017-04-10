07b087f89299eb6823a50f8f50862369ec9e609d
fifsky <fifsky@aliyun.com>
2017-04-10 09:55:09 +0800

    remove redis stat change redis reconnection mechanism

--- src/Very/Cache/Redis.php
+++ src/Very/Cache/Redis.php
@@ -11,10 +11,8 @@ namespace Very\Cache;
 
 class Redis
 {
-    //是否加入统计队列，因为刷新内存的代码并不需要加入统计队列，否则统计队列会撑挂掉
-    public  $is_stat = false;
-    private $config;//配置
-    private $node;//节点
+    private $config;
+    private $node;
 
     /**
      * @param $node
@@ -35,63 +33,47 @@ class Redis
         return $cache[$node];
     }
 
-    //是否开启监控统计
-    public function setStat($is_stat)
-    {
-        $this->is_stat = $is_stat;
-
-        return $this;
-    }
-
     private function connect($reconnect = false)
     {
         if (!$this->config) {
-            throw new \RuntimeException('redis config error.');
+            throw new \InvalidArgumentException('Redis connection not configured.');
         }
 
         static $redis_cache = [];
 
         if (!isset($redis_cache[$this->node]) || $reconnect) {
-            try {
-                $_start_time = microtime(true);
+            $client  = new \Redis();
+            $timeout = isset($this->config['timeout']) ? $this->config['timeout'] : 1;
+            $client->connect($this->config['host'], $this->config['port'], $timeout);
 
-                $client  = new \Redis();
-                $timeout = isset($this->config['timeout']) ? $this->config['timeout'] : 1;
-                $client->connect($this->config['host'], $this->config['port'], $timeout);
-
-                if (!empty($this->config['password'])) {
-                    $client->auth($this->config['password']);
-                }
-
-                if (!empty($this->config['database'])) {
-                    $client->select($this->config['database']);
-                }
-
-                if (!empty($this->config['prefix'])) {
-                    $client->setOption(\Redis::OPT_PREFIX, $this->config['prefix']);
-                }
-
-                if (!empty($this->config['read_timeout'])) {
-                    $client->setOption(\Redis::OPT_READ_TIMEOUT, $this->config['read_timeout']);
-                }
-
-                $redis_cache[$this->node] = $client;
-
-                if ($this->is_stat) {
-                    mstat()->set(1, 'Redis连接效率', mstat()->formatTime(number_format(microtime(true) - $_start_time, 6)),
-                        $this->config['host'] . ':' . $this->config['port']);
-                }
-            } catch (\RedisException $e) {
-                if ($this->is_stat) {
-                    mstat()->set(1, 'BUG错误', 'Redis连接错误', "{$this->config['host']}:{$this->config['port']}");
-                }
-                throw new \RedisException('redis connect error:' . $e->getMessage());
+            if (!empty($this->config['password'])) {
+                $client->auth($this->config['password']);
             }
+
+            if (!empty($this->config['database'])) {
+                $client->select($this->config['database']);
+            }
+
+            if (!empty($this->config['prefix'])) {
+                $client->setOption(\Redis::OPT_PREFIX, $this->config['prefix']);
+            }
+
+            if (!empty($this->config['read_timeout'])) {
+                $client->setOption(\Redis::OPT_READ_TIMEOUT, $this->config['read_timeout']);
+            }
+
+            $redis_cache[$this->node] = $client;
         }
 
         return $redis_cache[$this->node];
     }
 
+    /**
+     * @param \RedisException $e
+     *
+     * @return bool
+     * @deprecated
+     */
     private function isConnectionLost(\RedisException $e)
     {
         if (strpos($e->getMessage(), 'Redis server went away') !== false || strpos($e->getMessage(), 'Connection lost') !== false || strpos($e->getMessage(),
@@ -113,28 +95,19 @@ class Redis
      */
     public function __call($func, $params)
     {
-        $redis_server = $this->connect();
+        $reconnect = false;
         for ($i = 0; $i < 2; ++$i) {
             try {
-                $_start_time = microtime(true);
-                $ret         = call_user_func_array(array($redis_server, $func), $params);
-                if ($this->is_stat) {
-                    mstat()->set(1, 'Redis执行效率', mstat()->formatTime(number_format(microtime(true) - $_start_time, 6)),
-                        "{$this->config['host']}:{$this->config['port']}({$func})");
-                }
+                $redis_server = $this->connect($reconnect);
+                $ret          = call_user_func_array(array($redis_server, $func), $params);
             } catch (\RedisException $e) {
-                if ($this->is_stat) {
-                    mstat()->set(1, 'BUG错误', 'Redis执行错误', "{$this->config['host']}:{$this->config['port']}", $e->getMessage(), 0.1);
-                }
-
                 logger()->error('Redis exec error',
                     ["host" => "{$this->config['host']}:{$this->config['port']}", "msg" => $e->getMessage(), "file" => $e->getFile(), 'line' => $e->getLine()]);
-
-                if ($this->isConnectionLost($e)) {
-                    $redis_server = $this->connect(true);
+                if ($i == 0) {
+                    $reconnect = true;
                     continue;
                 } else {
-                    throw new \RedisException('redis execute error:' . $e->getMessage());
+                    throw $e;
                 }
             }
             break;
