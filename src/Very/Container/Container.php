6e3afe899a22c1a5d2ecb1715791b6aca8f1188a
fifsky <fifsky@aliyun.com>
2017-05-10 10:37:39 +0800

    update version

--- src/Very/Container/Container.php
+++ src/Very/Container/Container.php
@@ -597,8 +597,10 @@ class Container implements ArrayAccess
      */
     protected function resolve($abstract, $parameters = [])
     {
+        $abstract = $this->getAlias($abstract);
+
         $needsContextualBuild = ! empty($parameters) || ! is_null(
-            $this->getContextualConcrete($abstract = $this->getAlias($abstract))
+            $this->getContextualConcrete($abstract)
         );
 
         // If an instance of the type is currently being managed as a singleton we'll
